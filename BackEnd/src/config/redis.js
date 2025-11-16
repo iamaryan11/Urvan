4d767fc35c843567be649b9af51efc2dea979d86
Aryan <aryaneos11@gmail.com>
2025-11-16 23:52:53 +0530

    Update Redis client configuration details

--- BackEnd/src/config/redis.js
+++ BackEnd/src/config/redis.js
@@ -1,10 +1,10 @@
 const {createClient}=require('redis');
 const redisClient = createClient({
     username: 'default',
-    password: 'lBFoRywmWPxTFA7E0FUPlkMD9P3EMRJd', // error de rha tha env me we will see
+    password: 'CZIQRv8vgiCbO7oxzbcXM8G6BICqm4ey', // error de rha tha env me we will see
     socket: {
-        host:'redis-13157.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 13157,
+        host:'redis-13673.c212.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 13673,
     }
 });
 module.exports=redisClient;
